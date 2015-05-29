5ce3244bc5594362b43aca965167840c645d32b0
nselesky <nickolas.selesky@gmail.com>
2015-05-29 09:58:58 -0500

    adds gitignore

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'bundler'
 Bundler.require()
 
+Dotenv.load
+set :publishable_key, ENV['PUBLISHABLE_KEY']
+set :secret_key, ENV['SECRET_KEY']
+
+
 get '/' do
   erb :index
+end
+
+post '/charge' do
+  @amount = 1000
+
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :card => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+  :amount => @amount,
+  :currency => 'usd'
+  )
 end
